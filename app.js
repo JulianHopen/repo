005fbd50d7e1e7098e84fea60236a60d22eacd1a
JulianHopen <julianhopen@gmail.com>
2025-01-16 13:37:43 +0100

    Cleaned up the files and moved the password hashing to the services file into the addUser function

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -22,9 +20,8 @@ app.get("/signup", (req, res) => {
 app.post("/signup", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  bcrypt.hash(password, saltRounds, function (err, hashedPassword) {
-    addUser(email, hashedPassword);
-  });
+  addUser(email, password);
+
   return res.redirect("/signup");
 });
 
--- database/services.js
+++ database/services.js
@@ -1,11 +1,14 @@
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const { createConnection } = require("./database");
 
 async function addUser(email, password) {
   const connection = await createConnection();
+  const hashedPassword = bcrypt.hashSync(password, saltRounds);
   connection.connect();
 
   const query = "INSERT INTO user (email, password) VALUES (?,?)";
-  connection.execute(query, [email, password]);
+  connection.execute(query, [email, hashedPassword]);
 
   connection.end();
 }
