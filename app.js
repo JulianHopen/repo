4ec8595de24f9f6b803b4a8e1df58a20c02843cd
JulianHopen <julianhopen@gmail.com>
2025-01-30 14:11:23 +0100

    Added CSS to project

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const session = require("express-session");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(
   session({
