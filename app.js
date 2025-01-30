758bab05f640672f41bbcd46063adc6db5243279
JulianHopen <julianhopen@gmail.com>
2025-01-30 13:14:31 +0100

    Added authenticator and session

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const { addUser } = require("./database/services");
+const { addUser, authenticateUser } = require("./database/services");
 
 const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
+const validator = require("validator");
+const session = require("express-session");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
+app.use(
+  session({
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    secret: "shhhh, very secret",
+  })
+);
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -17,12 +26,63 @@ app.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-app.post("/signup", (req, res) => {
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/signup", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  addUser(email, password);
 
-  return res.redirect("/signup");
+  const strongPassword = validator.isStrongPassword(password, {
+    minLength: 3,
+    minLowercase: 0,
+    minUppercase: 0,
+    minNumbers: 0,
+    minSymbols: 0,
+  });
+
+  if (!strongPassword) {
+    console.log(validator.isStrongPassword(password));
+    return res.redirect("/signup");
+  }
+
+  const emailChecker = await addUser(email, password);
+
+  if (!emailChecker) {
+    console.log("Email is already in use");
+    return res.redirect("/signup");
+  }
+
+  return res.redirect("/login");
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const auth = await authenticateUser(email, password);
+
+  if (auth) {
+    req.session.email = auth.email;
+    return res.redirect("/dashboard");
+  }
+  return res.redirect("/login");
+});
+
+function isAuthenticated(req, res, next) {
+  if (req.session.email) {
+    next();
+  } else {
+    req.session.error = "Access denied!";
+    res.redirect("/login");
+  }
+}
+
+app.get("/dashboard", isAuthenticated, (req, res) => {
+  res.render("dashboard", {
+    email: req.session.email,
+  });
 });
 
 app.listen(port, () => {
--- database/services.js
+++ database/services.js
@@ -4,13 +4,38 @@ const { createConnection } = require("./database");
 
 async function addUser(email, password) {
   const connection = await createConnection();
-  const hashedPassword = bcrypt.hashSync(password, saltRounds);
+
   connection.connect();
 
-  const query = "INSERT INTO user (email, password) VALUES (?,?)";
-  connection.execute(query, [email, hashedPassword]);
+  const FindUserQuery = "SELECT * FROM user WHERE email = ?;";
+  const [rows] = await connection.execute(FindUserQuery, [email]);
+  const user = await rows[0];
 
+  if (user) {
+    return false;
+  }
+  const addUserQuery = "INSERT INTO user (email, password) VALUES (?,?)";
+  const hashedPassword = bcrypt.hashSync(password, saltRounds);
+  connection.execute(addUserQuery, [email, hashedPassword]);
+
+  connection.end();
+  return true;
+}
+
+async function authenticateUser(email, password) {
+  const connection = await createConnection();
+
+  connection.connect();
+  const query = "SELECT * FROM user WHERE email = ?";
+  const [rows] = await connection.execute(query, [email]);
+  const user = await rows[0];
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (match) {
+    return { success: true, email: user.email };
+  }
   connection.end();
 }
 
-module.exports = { addUser };
+module.exports = { addUser, authenticateUser };
